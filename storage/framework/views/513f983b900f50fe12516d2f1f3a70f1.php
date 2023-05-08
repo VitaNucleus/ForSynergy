b9b814c36c48776ad42e7c1bbcf038da747ab95b
Daniil <127922540+VitaNucleus@users.noreply.github.com>
2023-05-08 19:51:20 +0500

    Add files via upload

---
+++ storage/framework/views/513f983b900f50fe12516d2f1f3a70f1.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title></title>
+</head>
+<body>
+<h1>It is private page</h1>
+</body>
+</html><?php /**PATH /home/daniil/.config/composer/vendor/bin/Abiturient/resources/views/private.blade.php ENDPATH**/ ?>
